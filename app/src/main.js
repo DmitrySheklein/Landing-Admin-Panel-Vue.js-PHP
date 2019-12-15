d4a5c13e19a26258bd0f38ddcb5bae2489fe2e4a
Dmitry <sheklein.dima@gmail.com>
2019-12-15 21:00:36 +0300

    Настройка iframe

--- app/src/main.js
+++ app/src/main.js
@@ -1,46 +1,7 @@
-const Vue = require('vue/dist/vue.js');
-const axios = require('axios');
+const Editor = require('./editor')
 
-new Vue({
-    el: '#app',
-    data: {
-        pageName: '',
-        pageList: []
-    },
-    created() {
-        this.updatePageList();
-    },
-    methods: {
-        createPage(){
-            console.log(this.pageName);
-            axios.post('./api/createNewHtmlPage.php', {
-                'name': this.pageName
-            })
-            .then((response)=>{
-                this.updatePageList();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });            
-        },
-        deletePage(page){
-            axios.post('./api/removeHtmlPage.php', {
-                'name': page
-            })
-            .then((response)=>{
-                console.log(response)
-                this.updatePageList();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });             
-            
-        },
-        updatePageList(){
-            axios.get('./api')
-                .then((response)=>{
-                    this.pageList = response.data
-                })
-        }
-    },
-})
+window.editor = new Editor();
+
+window.onload = () => {
+    window.editor.open('index.html')
+}
